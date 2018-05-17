7d2ca97a39185e83adc24a37359645a52d7b27d6
ga97jod <ga97jod@mytum.de>
2018-05-17 12:05:53 +0200

    Implemented detector using an gaussian mixture model based background detector to define regions of interest

--- As301_detector.py
+++ As301_detector.py
@@ -30,7 +30,7 @@ import As301_classifier
 
 ########################################################################
 
-INPUT_FILEPATH = Path("./inputs/videos/Cars_01.mov")
+INPUT_FILEPATH = Path("./inputs/videos/Cars_05.mov")
 FILENAME = INPUT_FILEPATH.stem
 
 ########################################################################
@@ -43,7 +43,7 @@ FILENAME = INPUT_FILEPATH.stem
 
 # script for sliding window from "Sliding Windows for Object Detection with Python and OpenCV"
 # (see https://www.pyimagesearch.com/2015/03/23/sliding-windows-for-object-detection-with-python-and-opencv/)
-def sliding_window(image, stepSize=(24,24), windowSize=(64,64)):
+def sliding_window(image, stepSize=(16,16), windowSize=(64,64)):
     # slide a window across the image
     for y in range(0, image.shape[0], stepSize[0]):
         for x in range(0, image.shape[1], stepSize[1]):
@@ -57,7 +57,7 @@ def detectCars(frame):
 
     # store the frame in different dimensions 
     # and begin with the lowest resolution
-    scaled_frames = list(pyramid_gaussian(frame, downscale=1.2, max_layer=8))
+    scaled_frames = list(pyramid_gaussian(frame, downscale=1.5, max_layer=3))
     scaled_frames = list(reversed(scaled_frames))
 
     detected_cars = []
@@ -69,13 +69,8 @@ def detectCars(frame):
         # loop over window in the image
         scaled_height, scaled_width = image_scaled.shape[:2]
         SCALING_FACTOR = (org_height / scaled_height + org_width / scaled_width) / 2.0
-        # print((scaled_height, scaled_width))
-        # print("Scaling Factor : {}".format(SCALING_FACTOR))
-
+        
         if scaled_height < 64 or scaled_width < 64:
-            continue
-
-        if scaled_height > 600 or scaled_width > 1100:
             continue
 
         for (x, y, image_window) in sliding_window(image_scaled):
@@ -112,34 +107,32 @@ def detectCars(frame):
                 BORDER_PIXELS = 30
                 isOverlapping = False
 
+                currRect = (        int(x*SCALING_FACTOR),
+                                    int(y*SCALING_FACTOR),
+                                    int((x + 64) * SCALING_FACTOR),
+                                    int((y + 64) * SCALING_FACTOR) )
+
                 for (prev_x,prev_y,prev_scale) in detected_cars:
-                    prev_bounds_x = (int(prev_x * prev_scale), int((prev_x + 64) * prev_scale) ) 
-                    prev_bounds_y = (int(prev_y * prev_scale), int((prev_y + 64) * prev_scale) ) 
 
-                    overlapCheck = (
-                                    int(x*SCALING_FACTOR) > prev_bounds_x[0] - BORDER_PIXELS and
-                                    int((x + 64) * SCALING_FACTOR) < prev_bounds_x[1] + BORDER_PIXELS and
-                                    int(y*SCALING_FACTOR) > prev_bounds_y[0] - BORDER_PIXELS and
-                                    int((y + 64) * SCALING_FACTOR) < prev_bounds_y[1] + BORDER_PIXELS
-                                    )
+                    oldRect = (     int(prev_x * prev_scale),
+                                    int(prev_y * prev_scale),
+                                    int((prev_x + 64) * prev_scale),
+                                    int((prev_y + 64) * prev_scale) )
 
-                    if overlapCheck:
+                    if rectangleOverlap(rect1 = oldRect, rect2 = currRect, margin = BORDER_PIXELS):
                         isOverlapping = True
-                        # print("image overlaps {0} , {1}".format(prev_bounds_x, x*SCALING_FACTOR))
+                        break
 
                 if not isOverlapping:
                     detected_cars.append((x,y,SCALING_FACTOR))
 
     res_image = frame.copy()
     for (x,y,scale) in detected_cars:
-        # cv2.rectangle(res_image, 
-        #     (int(x * scale), int(y * scale)) , 
-        #     (int((x+64)*scale),int((y+64)*scale)), (0,255,0),2)
         rectangles.append((
                     int(x * scale),
                     int(y * scale),
-                    int(64*scale),
-                    int(64*scale)
+                    int(64 * scale),
+                    int(64 * scale)
             ))
 
     return rectangles
@@ -157,9 +150,25 @@ def backgroundDetection(frame):
             continue
         x,y,w,h = cv2.boundingRect(cnt)
         rectangles.append((x,y,w,h))
-        # cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2)
 
     return rectangles
+
+# returns True if (x1,y1,a1,b1) encloses (x2,y2,a2,b2) with a certain 
+# (x,y) = coords of the top left corner point
+# (a,b) = coords of the bottom right corner point
+# margin of pixel allowance
+def rectangleOverlap(rect1 = (0,0,0,0), rect2 = (0,0,0,0), margin=0):
+    x1, y1, a1, b1 = rect1
+    x2, y2, a2, b2 = rect2
+
+    overlapCheck = (
+                    x2 > x1 - margin and
+                    a2 < a1 + margin and
+                    y2 > y1 - margin and
+                    b2 < b1 + margin
+                    )
+
+    return overlapCheck 
 
 # Setup Video
 
@@ -183,45 +192,62 @@ while True:
     if not retval:
         break
 
+    # Resize the frame.
+    scaleX, scaleY = (0.5,0.5)
+    frame = cv2.resize(frame, (0, 0), fx=scaleX, fy=scaleY)
+
     bgRectangles = backgroundDetection(frame)
+
+    # remove too small rectangles
+    bgRectangles = [(x,y,w,h) for (x,y,w,h) in bgRectangles if (w*h > 1500 and w*h < 65000)]
+    # remove rectangles that aren't square
+    bgRectangles = [(x,y,w,h) for (x,y,w,h) in bgRectangles if ((w/h > 0.2) if h > w else (h/w > 0.2))]
+
+    bg_rect_overlap_free = []
+
+    # remove overlapping rectangles
+    for (x1,y1,w1,h1) in bgRectangles:
+        isOverlapping = False
+        for (x2,y2,w2,h2) in bgRectangles:
+            if (
+                (x1,y1,w1,h1) != (x2,y2,w2,h2) and 
+                rectangleOverlap((x2,y2,x2+w2,y2+h2), (x1,y1,x1+w1,y1+h1), margin=50)
+                ):
+                isOverlapping = True
+                break
+        if not isOverlapping:
+            bg_rect_overlap_free.append((x1,y1,w1,h1))
 
     detectedRect = []
 
     # (x,y,w,h)
-    for (x,y,w,h) in bgRectangles:
-        if w*h < 10000:
-            continue
-        print(x,y,w,h)
-        bound_x = 50 if x < 50 else x
-        bound_y = 50 if y < 50 else y
+    for (x,y,w,h) in bg_rect_overlap_free:
+        PIXEL_BOUND = 40
+        bound_x = PIXEL_BOUND if x >= PIXEL_BOUND else x
+        bound_y = PIXEL_BOUND if y >= PIXEL_BOUND else y
+        # print((y-bound_y,y+h+bound_y,x-bound_x,x+w+bound_x))
+        cv2.rectangle(frame, (x-bound_x, y-bound_y), (x+w+bound_x, y+h+bound_y), (0,0,255), 2 )
         detections = detectCars(frame[y-bound_y:y+h+bound_y,x-bound_x:x+w+bound_x,:])
         detections = [(x1+(x-bound_x),y1+(y-bound_y),w1,h1) for (x1,y1,w1,h1) in detections]
         detectedRect += detections
 
-    for (x,y,w,h) in detectedRect:
-        cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2)
+    # print("Detections before overlap {0}".format(len(detectedRect)))
+    detect_count = 0
+    for (x1,y1,w1,h1) in detectedRect:
+        isOverlapping = False
+        for (x2,y2,w2,h2) in detectedRect:
+            if (
+                (x1,y1,w1,h1) != (x2,y2,w2,h2) and 
+                rectangleOverlap((x2,y2,x2+w2,y2+h2), (x1,y1,x1+w1,y1+h1), margin=50)
+                ):
+                isOverlapping = True
+                break
 
-    # Resize the frame.
-    # scaleX, scaleY = (0.5,0.5)
-    # frame = cv2.resize(frame, (0, 0), fx=scaleX, fy=scaleY)
+        if not isOverlapping:
+            detect_count += 1
+            cv2.rectangle(frame,(x1,y1),(x1+w1,y1+h1),(0,255,0),2)
 
-    # As 3.02. (j) : Define the ROI
-    # only analyze the road in the video, not the grass
-    # if FILENAME == "Cars_01":
-    #     frame[:,int(200*scaleX):int(875*scaleX),:] = detectCars(frame[:,int(200*scaleX):int(875*scaleX),:])
-    # elif FILENAME == "Cars_02":
-    #     frame[:int(600*scaleY),:int(550*scaleX),:] = detectCars(frame[:int(600*scaleY),:int(550*scaleX),:])
-    #     frame[:,int(400*scaleX):int(1400*scaleX),:] = detectCars(frame[:,int(400*scaleX):int(1400*scaleX),:])
-    # elif FILENAME == "Cars_03":
-    #     frame[:,int(500*scaleX):int(1700*scaleX),:] = detectCars(frame[:,int(500*scaleX):int(1700*scaleX),:])
-    # elif FILENAME == "Cars_04":
-    #     frame[:int(450*scaleY),:int(650*scaleX),:] = detectCars(frame[:int(450*scaleY),:int(650*scaleX),:])
-    #     frame[:,int(400*scaleX):int(1500*scaleX),:] = detectCars(frame[:,int(400*scaleX):int(1500*scaleX),:])
-    # elif FILENAME == "Cars_05":
-    #     frame[int(300*scaleY):,int(50*scaleX):int(800*scaleX),:] = detectCars(frame[int(300*scaleY):,int(50*scaleX):int(800*scaleX),:])
-    #     frame[int(300*scaleY):,int(950*scaleX):int(1750*scaleX),:] = detectCars(frame[int(300*scaleY):,int(950*scaleX):int(1750*scaleX),:])
-    # else:
-    #     frame = detectCars(frame)
+    # print("Detections after overlap {0}".format(detect_count))
 
     # Display the resulting frame.
     cv2.imshow("Video", frame)
